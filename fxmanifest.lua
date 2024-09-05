01bdb0a221b729c61947026095d550ae0d12b056
The Mystic G <35405638+themysticg@users.noreply.github.com>
2024-09-05 22:08:48 +0100

    1st commit with code

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+-- https://Discord.me/3qshop
+fx_version 'adamant'
+
+game 'gta5'
+version '1.0.0'
+server_scripts {
+  --'server/server.lua',
+}
+
+client_scripts {
+  'client/main.lua',
+}
+
+ui_page {
+  'html/index.html'
+}
+
+files {
+  'html/index.html',
+  'html/style.css',
+  'html/app.js'
+}
